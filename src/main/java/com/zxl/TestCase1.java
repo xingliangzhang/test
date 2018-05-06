da988b8dbcf3bfa2018a80bb8ea6d88c59078e21
zhangxingliang <18239775215@163.com>
2018-05-06 18:05:29 +0800

    feature 提交格式化

--- src/main/java/com/zxl/TestCase1.java
+++ src/main/java/com/zxl/TestCase1.java
@@ -5,13 +5,10 @@ public class TestCase1 {
 
 	public static void test2() {
 		System.out.println("改变2");
-
 	}
-
 	public static void test1() {
 		int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 		int[] b = new int[] { 1, 2, 3, 4, 5 };
-
 		end: // 跳出多层循环
 		for (int i = 0; i < a.length; i++) {
 			System.out.println("i=" + i);

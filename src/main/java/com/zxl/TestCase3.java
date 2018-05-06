f8816597dba9ca15f424724f9ec2a2a049e1db0b
zhangxingliang <18239775215@163.com>
2018-05-06 18:44:16 +0800

    feature 分支 添加方法 TestCase3

--- src/main/java/com/zxl/TestCase3.java
+++ src/main/java/com/zxl/TestCase3.java
@@ -1,9 +1,22 @@
 package com.zxl;
 
 public class TestCase3 {
+	public static String test1() {
+		return "张星亮";
+
+	}
+	public static String test2() {
+		String s=test1();
+		if("zhang".equals(s)) {
+			System.out.println(111);
+		}
+		return "张星亮";
+		
+	}
+
 	public static void main(String[] args) {
+		System.out.println("我是合并后的");
 		System.out.println("我是合并后的提交3张星亮不去掉");
 	}
-	
 
 }

7c8bbbe8e0fb943bef0c7389fa147d387650bff7
zhangxingliang <18239775215@163.com>
2018-05-06 17:51:09 +0800

    master 分支 改变3 提交 增加打印

--- src/main/java/com/zxl/TestCase1.java
+++ src/main/java/com/zxl/TestCase1.java
@@ -4,20 +4,20 @@ public class TestCase1 {
 	public static void test1() {
 		int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 		int[] b = new int[] { 1, 2, 3, 4, 5 };
-		
-		end://跳出多层循环
+
+		end: // 跳出多层循环
 		for (int i = 0; i < a.length; i++) {
-			//改变start
-			if(a[i]==1) {
+			// 改变start
+			if (a[i] == 1) {
 				System.out.println("我是中国人");
 			}
-			//改变end
-			System.out.println("i="+i);
+			// 改变end
+			System.out.println("i=" + i);
 			for (int j = 0; j < b.length; j++) {
-				//System.err.println("j="+j);
-				if(b[j]<4) {
-					System.out.println("b[j]="+b[j]);
-				}else {
+				// System.err.println("j="+j);
+				if (b[j] < 4) {
+					System.out.println("b[j]=" + b[j]);
+				} else {
 					System.out.println("结束循环");
 					break end;
 				}
@@ -27,5 +27,7 @@ public class TestCase1 {
 
 	public static void main(String[] args) {
 		TestCase1.test1();
+		System.out.println("改变3");
+
 	}
 }

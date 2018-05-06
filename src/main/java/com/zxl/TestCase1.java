a58ff777760e1c6f50f91434348d1d05329b600e
zhangxingliang <18239775215@163.com>
2018-05-06 17:33:19 +0800

    测试项目，git命令测试

---
+++ src/main/java/com/zxl/TestCase1.java
@@ -0,0 +1,26 @@
+package com.zxl;
+
+public class TestCase1 {
+	public static void test1() {
+		int[] a = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+		int[] b = new int[] { 1, 2, 3, 4, 5 };
+		
+		end://跳出多层循环
+		for (int i = 0; i < a.length; i++) {
+			System.out.println("i="+i);
+			for (int j = 0; j < b.length; j++) {
+				//System.err.println("j="+j);
+				if(b[j]<4) {
+					System.out.println("b[j]="+b[j]);
+				}else {
+					System.out.println("结束循环");
+					break end;
+				}
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		TestCase1.test1();
+	}
+}
